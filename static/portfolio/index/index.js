12619d29abcca0f336865b6ed39d56c30fddb6c1
Mikel <mikel.valencia02@gmail.com>
2023-08-20 23:05:17 +0200

    Added technical skills in about page. Improved the responsive design.

--- static/portfolio/index/index.js
+++ static/portfolio/index/index.js
@@ -1,9 +1,9 @@
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 const path = document.getElementById("splash-screen-logo-path");
 
-tl.to(path, { Animation: "none", delay: 1.5 });
-tl.to(".splash-screen", { y: "-100%", duration: .8 });
-tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: .3 });
+// tl.to(path, { Animation: "none", delay: 1.75 });
+// tl.to(".splash-screen", { y: "-100%", duration: .8, delay: 1.9 });
+tl.fromTo("nav", { opacity: 0 }, { opacity: 1, duration: .3, delay: 2.7 });
 tl.fromTo(".intro", { opacity: 0 }, { opacity: 1, duration: .3 }, "-=.3");
 tl.fromTo(".svg-separator", { opacity: 0 }, { opacity: 1, duration: .3 });
 tl.fromTo(".description", { opacity: 0 }, { opacity: 1, duration: .3 });
