f4be354f6f81629031e7b428ac8c601454837574
Mikel <mikel.valencia02@gmail.com>
2023-08-27 20:46:38 +0200

    Reorganized the portfolio into a single page app. Added Projects section. Fixed some responsiveness issues. Fixed an issue of navbar blurring. Modifiedand improved completely the hero section.

--- static/portfolio/index/index.js
+++ static/portfolio/index/index.js
@@ -1,3 +1,7 @@
+// Initiallize Bootstrap tooltips.
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
 const tl = gsap.timeline({ defaults: { ease: "power1.out" } });
 const path = document.getElementById("splash-screen-logo-path");
 
@@ -8,3 +12,26 @@ tl.fromTo(".intro", { opacity: 0 }, { opacity: 1, duration: .3 }, "-=.3");
 tl.fromTo(".svg-separator", { opacity: 0 }, { opacity: 1, duration: .3 });
 tl.fromTo(".description", { opacity: 0 }, { opacity: 1, duration: .3 });
 tl.fromTo(".social-links", { opacity: 0 }, { opacity: 1, duration: .3 });
+
+// Gmail copy to clipboard.
+const gmailBtn = document.getElementById("gmail-btn");
+
+function copyGmail() {
+
+    navigator.permissions.query({ name: "write-on-clipboard" }).then((result) => {
+
+        if (result.state == "granted" || result.state == "prompt") {
+
+            var gmail = document.getElementById("gmail").innerHTML;
+            
+            navigator.clipboard.writeText(gmail).then(() => {
+                navigator.clipboard.writeText(gmail);
+            },() => {
+                console.error('Failed to copy: ', err);
+            });
+
+        }
+
+    });
+
+}
